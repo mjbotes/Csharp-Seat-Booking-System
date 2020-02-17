e4039ac4191af6478c58fbcb660398e53c8c5635
Marnus Botes <marnusb@bbd.co.za>
2020-02-17 09:30:32 +0200

    Merge backup

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -15,7 +15,7 @@ namespace Csharp_Seat_Booking_System.Controllers
 
         public IActionResult ViewEvents(){
         EventList.Add(new Event("https://www.gstatic.com/tv/thumb/v22vodart/3542039/p3542039_v_v8_ac.jpg", "Avatar", "Its in IMAX"));
-        EventList.Add(new Event("default", "Jexi", "Its in IMAX"));
+        EventList.Add(new Event("https://lh3.googleusercontent.com/proxy/3CdejYgpHivKQgMH8mICvlDUEwluwy5fDbNnpFilOqcTIX72Yhc8CPm-iABdRzGS-80T091jqV3frPP9W8tRikb0nEJHkIDPxq1_", "Jexi", "Its in IMAX"));
         EventList.Add(new Event("default", "Terminator", "Its in IMAX"));
         ViewData["Events"] = EventList.ToArray();
         return View();
@@ -34,6 +34,19 @@ namespace Csharp_Seat_Booking_System.Controllers
                 Console.Out.WriteLine(Test);
             }
             ViewData["arr"] = TestList;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSeatsToCart(string SeatListJson)
+        {
+            Console.WriteLine("im HERTR");
+            if (SeatListJson == null)
+            {
+                return BadRequest();
+            }
+
+            Console.Out.WriteLine(SeatListJson);
             return View();
         }
     }
