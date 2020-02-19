ff8e275cf25cae44c243864d712dd3ff10f3f9ae
Ferronique May <ferronique@bbd.co.za>
2020-02-19 09:05:50 +0200

    Checkout recognizes current logged in user

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,5 +13,61 @@ namespace Csharp_Seat_Booking_System.Controllers
     public class CheckoutController : Controller
     {
 
+        public string EventName = "Queen and Slim";
+        public string Venue = "Cresta Sterkenikor";
+        public int SeatCategoryGold = 5;
+        public int SeatCategoryOrange = 5;
+        public int SeatCategoryBlue = 5;
+        public int SeatCategoryGoldPrice = 200;
+        public int SeatCategoryOrangePrice = 150;
+        public int SeatCategoryBluePrice = 100;
+        
+        public int NumberOfTickets;
+        public int PriceOfTickets;
+
+
+        [DataType(DataType.Date)]
+        public DateTime BookingDate = DateTime.Now;
+       
+        [DataType(DataType.Time)]
+        public DateTime BookingTime = DateTime.Now;
+        public IActionResult Checkout()
+        {
+        
+            ViewData["EventName"] = EventName;
+            ViewData["BookingDate"] = BookingDate.ToString("dddd, dd MMMM yyyy");
+            ViewData["BookingTime"] = BookingTime.ToString("HH:mm");
+            ViewData["SeatCategoryGold"] = SeatCategoryGold;
+            ViewData["SeatCategoryOrange"] = SeatCategoryOrange;
+            ViewData["SeatCategoryBlue"] = SeatCategoryBlue;
+            ViewData["SeatCategoryGoldPrice"] = SeatCategoryGoldPrice;
+            ViewData["SeatCategoryOrangePrice"] = SeatCategoryOrangePrice;
+            ViewData["SeatCategoryBluePrice"] = SeatCategoryBluePrice;
+
+            return View();
+        }
+        public IActionResult ViewCart()
+        {
+        
+            ViewData["EventName"] = EventName;
+            ViewData["Venue"] = Venue;
+            ViewData["BookingDate"] = BookingDate.ToString("dddd, dd MMMM yyyy");
+            ViewData["BookingTime"] = BookingTime.ToString("HH:mm");
+            ViewData["NumberOfTickets"] = SeatCategoryGold + SeatCategoryOrange + SeatCategoryBlue;
+            ViewData["PriceOfTickets"] = (SeatCategoryGold*SeatCategoryGoldPrice) + (SeatCategoryOrange*SeatCategoryOrangePrice) + (SeatCategoryBlue*SeatCategoryBluePrice);
+
+            return View();
+        }
+
+        public IActionResult PaymentSuccess()
+        {
+            
+            return View();
+        }
+
+        public IActionResult PaymentFailed()
+        {
+            return View();
+        }
     }
 }
